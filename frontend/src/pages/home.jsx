e81a5497c8be567d9c04f543b3462530d4f26590
Arafat Rahman <arafatrahman0092@gmail.com>
2026-07-15 20:00:34 +0600

    new update

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/hero"
+import Stats from "../components/state";
+import Features from "../components/features";
+
+
+const Home = () => {
+    return( 
+        <div>
+            <Hero/>
+            <Stats/>
+            <Features/>
+           
+
+        </div>
+    );
+}
+export default Home;
